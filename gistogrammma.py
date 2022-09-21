2966efcd49109204b2dee981f58e95f8ad6cadb7
draka <drakanoy@gmail.com>
2022-09-21 21:54:10 +0300

    нормальное распределенние массы (по идее)

--- gistogrammma.py
+++ gistogrammma.py
@@ -9,13 +9,13 @@ massy = [4.47, 4.38, 4.52, 5.08, 4.72, 4.51, 4.41, 4.42, 4.67, 4.6, 4.52, 4.15, 
          4.46, 4.49, 4.4, 4.53, 4.53, 4.46]
 G = 0.174
 x_sred = 4.522
-##p = lambda x: (1 / (G* (2 * pi) ** 0.5)) * np.exp(-1 * ((x - x_sred) ** 2 / (2 * G ** 2)))
-##l_p = []
-##for i in massy:
-##    l_p.append(i)
+p = lambda x: (1 / (G* (2 * pi) ** 0.5)) * np.exp(-1 * ((x - x_sred) ** 2 / (2 * G ** 2)))
+l_p = []
+for i in massy:
+    l_p.append(p(i))
 # plt.hist(massy, 5, label='гистограмма')
 plt.title(label='Распределение масс грузиков', loc='center', fontweight='regular')
-##plt.plot(massy, l_p)
+plt.plot(massy, l_p)
 ax.grid()
 ax.set_xlabel('Масса, г')
 sns_plot = sns.distplot(massy, hist=True, kde=True,
