f75eb533acb0a7a18216d14e86ff3455f3fa2032
draka <drakanoy@gmail.com>
2022-10-28 17:19:29 +0300

    погрешности для оборотного маятника (2) квадратичная апроксимация

--- момент_инерции.py
+++ момент_инерции.py
@@ -16,23 +16,25 @@ I_exper = lambda T2: (M * g * l / (4 * pi ** 2)) * T2
 I_exp_list = [I_exper(i) for i in T2]
 print(I_exp_list)
 fig, ax = plt.subplots()
-# z = np.polyfit(t, I_exp_list, 1)
-# p = np.poly1d(z)
-# print(p)
-# y1 = p(t)
+z = np.polyfit(r_t, I_exp_list, 2)
+p = np.poly1d(z)
+print(p)
+y1 = p(r_t)
+print(y1)
+plt.plot(r, p(r), '-g', label='fufufu')
 plt.plot(r_t, I_exp_list, '.', label='Экспериментальные точки')
 plt.plot(r, I_teor(r), '-r', label='Теоретическая зависимость')
 plt.title(label='Зависимость момента инерции физического маятника \n от расстояния между точкой А и грузиками',
           loc='center', fontweight='regular')
 ax.grid()
-plt.errorbar(r_t, I_exp_list, yerr=[0.006648693668638697,
+"""plt.errorbar(r_t, I_exp_list, yerr=[0.006648693668638697,
                                     0.006651339594558886,
                                     0.00667356537228847,
                                     0.006695261964834015,
                                     0.0067883985572246425,
                                     0.006902702556976773,
-                                    0.007341926259727972], fmt='b.')
+                                    0.007341926259727972], fmt='b.')"""
 ax.set_xlabel('расстояние от точки А до грузиков r, см')
-ax.set_ylabel('момент инерции, кг * м$^-$$^2$')
+ax.set_ylabel('момент инерции I, кг * м$^-$$^2$')
 plt.legend()
 plt.show()
