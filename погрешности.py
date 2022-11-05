a27eedb4c0975e52d427b695867ebf6896b7ea5f
draka <drakanoy@gmail.com>
2022-11-06 00:45:25 +0300

    погрешности для скорости пули

--- погрешности.py
+++ погрешности.py
@@ -1,13 +1,34 @@
-from sympy import symbols, init_printing, diff, sin
+from sympy import symbols, init_printing, diff, cos
+from math import radians
+from math import cos as cs
+from math import sin as sn
 
-a1, a2, t1, t2 = symbols('a1, a2, t1, t2')
+
+mc = 11.52 * 10 ** -3
+ac = radians(15.20)
+da_c = radians(3.4076)
+l, a, M, m, g, x = symbols('l, a, M, m, g, x')
 init_printing(use_unicode=True)
 
-print(diff((a1 ** 2 - a2 ** 2) / (a1 * t1 ** 2 - a2 * t2 ** 2), a1))
-print(diff((a1 ** 2 - a2 ** 2) / (a1 * t1 ** 2 - a2 * t2 ** 2), a2))
-print(diff((a1 ** 2 - a2 ** 2) / (a1 * t1 ** 2 - a2 * t2 ** 2), t1))
-print(diff((a1 ** 2 - a2 ** 2) / (a1 * t1 ** 2 - a2 * t2 ** 2), t2))
-
+print(diff((1 + M / m) * (x * g * l * (1 - cos(a))) ** 0.5, a))
+g = 9.8
+x = 2
+M = 0.09882 + mc
+m = mc
+a = ac
+l = 0.285
+dv_dM_c = (g*l*x*(1 - cs(a)))**0.5/m
+print(dv_dM_c)
+dv_dm_c = -M*(g*l*x*(1 - cs(a)))**0.5/m**2
+print(dv_dm_c)
+dv_dl_c = 0.5*(g*l*x*(1 - cs(a)))**0.5*(M/m + 1)/l
+print(dv_dl_c)
+dv_da_c = 0.5*(g*l*x*(1 - cs(a)))**0.5*(M/m + 1)*sn(a)/(1 - cs(a))
+print(dv_da_c)
+dm = 0.01 * 10 ** -3
+dM = (2) ** 0.5 * dm
+dl = (0.001 ** 2 + 0.0005 ** 2) ** 0.5
+da = da_c
+Δv = ((dv_dM_c * dM) ** 2 + (dv_dm_c * dm) ** 2 + (dv_dl_c * dl) ** 2 + (dv_da_c * da) ** 2) ** 0.5
 print()
-print()
-print(diff(a1 / t1 ** 2, t1))
+print(Δv)
