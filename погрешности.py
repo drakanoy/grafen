f1c6addad398103a7e5b2275c809290a1a9d8c47
draka <drakanoy@gmail.com>
2023-04-11 22:49:03 +0300

    Миликен

--- погрешности.py
+++ погрешности.py
@@ -4,7 +4,7 @@ from math import cos as cs
 from math import sin as sn
 
 
-l1, B, l2, t1, t2, r0 = symbols('l1, B, l2, t1, t2, r0')
+q0, r, A = symbols('q0, r, A')
 init_printing(use_unicode=True)
 
-print(diff(B*(l2/t2)**0.5, t2))
+print(diff(q0*(1+A/r)**(-1.5), r))
