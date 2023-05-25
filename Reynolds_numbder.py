2d7f9c5d3b282af9b2b0ce02b87672073b7579ff
draka <drakanoy@gmail.com>
2023-05-25 17:15:09 +0300

    работа с проектом считаем числа Рейнольдса для воды

--- Reynolds_numbder.py
+++ Reynolds_numbder.py
@@ -6,7 +6,7 @@ nu = 1.004 * 10 ** -6  # кинематическая вязкость м2/с
 b = 75 * 10 ** (-3)  # м
 a = 49 * 10 ** (-3)  # м
 
-w = np.arange(0, 0.9, 0.001)
+w = np.arange(0, 1.5, 0.001)
 
 Re550 = lambda wd: 550 + 0 * wd
 RE57 = lambda wd: 57 + 0 * wd
@@ -20,12 +20,12 @@ Ta = lambda w: w ** 2 * a * (b - a) ** 3 / (nu ** 2)
 
 fig, ax = plt.subplots()
 
-#plt.plot(w, Re(w) * ((b - a) / a), '-r', label='мое')
+plt.plot(w, Re(w) * ((b - a) / a), '-r', label='мое')
 plt.plot(w, (Ta(w))**0.5, '-y', label='Елены')
 # plt.plot(w, Re550(w), '-g', label='верхний предел \n Ω = 0.254 c$^-$$^1$ \n Ω = 1.56 об/мин')
 # plt.plot(w, Re57(w), '-b', label='нижний предел \n Ω = 0.026 c$^-$$^1$ \n Ω = 15.24 об/мин')
-#plt.plot(w, Ta400(w), '-g', label='верхний предел \n Ω = 0.254 c$^-$$^1$ \n Ω = 1.56 об/мин')
-plt.plot(w, Ta41(w), '-b', label='нижний предел')
+plt.plot(w, Ta400(w), '-g', label='верхний предел')
+#plt.plot(w, Ta41(w), '-b', label='нижний предел')
 
 plt.title(label='число Тейлора', loc='center', fontweight='regular')
 
