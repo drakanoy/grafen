64e38d57ef62f1098db37fcf8c3769583d0e5029
drakanoy <drakanoy@gmail.com>
2025-09-26 20:29:43 +0300

    хим потенциал

--- chemical_pothenchial.py
+++ chemical_pothenchial.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 def mu(Eg, T):
     return - Eg/2 + 3*np.log(3)/4 * T
 
+def n(Eg, T):
+    return 2*(T)**(3/2)*np.exp(-Eg/T)
+
 fig, ax = plt.subplots()
-x = np.linspace(0, 1, 10000)
-y = mu(1, x)
-plt.plot(x, y, '-r', label='$\\mu$ при $E_g = 1 eV$')
-plt.title(label='$\\mu$ при $E_g = 1 eV$', loc='center', fontweight='regular')
+T = np.linspace(0, 1, 10000)
+y = mu(1, T)
+y_1 = n(1, T)
+plt.plot(T, y_1, '-r', label='$n$ и $p$')
+plt.title(label='$n$ и $p$', loc='center', fontweight='regular')
 ax.grid()
-ax.set_xlabel('$kT$ $s(eV)$')
-ax.set_ylabel('$\\mu$ $(eV)$')
+ax.set_xlabel('$kT$ $(eV)$')
+ax.set_ylabel('$n$ и $p$ $(cm^{-3})$')
 plt.legend()
 plt.show()
