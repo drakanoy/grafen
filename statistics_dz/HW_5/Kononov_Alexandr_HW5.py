24d2c0a12f1864308d29a67f62f6a5c517e80e76
drakanoy <drakanoy@gmail.com>
2025-12-25 22:54:19 +0300

    статистика дз 5

--- statistics_dz/HW_5/Kononov_Alexandr_HW5.py
+++ statistics_dz/HW_5/Kononov_Alexandr_HW5.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import corner
+import matplotlib.pyplot as plt
 
 
 
@@ -7,60 +9,45 @@ data = np.loadtxt('5.dat')
 n = len(data)
 x, y, s_obs = data.T
 alpha = 0.05
-
-# модель A*np.sin(w*x) + B*np.cos(w*x)
-
-# Приоры
-# быстрый поиск по сетке для w0
-def fit_beta_w(w, tau2_ridge=1e12):
-    X = np.column_stack([np.sin(w*x), np.cos(w*x)])
-    X_t = X / s_obs[:, None]
-    y_t = y / s_obs
-    XtX = X_t.T @ X_t + np.eye(2) / tau2_ridge
-    Xty = X_t.T @ y_t
-    beta = np.linalg.solve(XtX, Xty)
-    resid = y - X @ beta
-    S = np.sum((resid / s_obs) ** 2)
-    return beta, S
-
-
-ws = np.linspace(0.05, 5.0, 4000)
-Ss = np.array([fit_beta_w(w)[1] for w in ws])
-w0 = float(ws[np.argmin(Ss)])
-
-#приор для w
-mu_logw = np.log(w0)
-sd_logw = 1.0
-
-def logprior_logw(logw):
-    return -0.5*((logw-mu_logw)/sd_logw)**2 - np.log(sd_logw*np.sqrt(2*np.pi))
-#приорs для A, B
 a0, b0 = 1.0, 1.0
 tau = 5.0
-# A ~ N(0, tau**2)    B ~ N(0, tau**2)
-# betta = (A, B)
+w_min, w_max = 0.05, 5.0
+rng = np.random.default_rng()
+
+# модель A*np.sin(w*x) + B*np.cos(w*x)
+def r(w):
+    return np.column_stack([np.sin(w*x), np.cos(w*x)])
+
+
+# Приоры
+w = rng.uniform(w_min, w_max)                 # приор для w
+beta = rng.normal(loc=0.0, scale=tau, size=2) # A ~ N(0, tau**2)    B ~ N(0, tau**2) beta = (A, B)
+sigma2 = 1.0 / rng.gamma(shape=a0, scale=1.0/b0)  # 1/ Gamma(a0,b0)
 
 
 # Монте-Карло
-rng = np.random.default_rng()
+
+def loglik(w, beta, sigma2):
+    X = r(w)
+    resid = y - X @ beta
+    return -0.5*np.sum(
+        np.log(2*np.pi*sigma2*(s_obs**2)) + (resid**2)/(sigma2*(s_obs**2))
+    )
 
 def sample_beta(w, sigma2):
-    X = np.column_stack([np.sin(w*x), np.cos(w*x)])
+    X = r(w)
     X_t = X / s_obs[:, None]
     y_t = y / s_obs
-    Prec = (X_t.T @ X_t) / sigma2 + np.eye(2) / tau**2
+
+    Prec = (X_t.T @ X_t) / sigma2 + np.eye(2) / (tau**2)
     Cov = np.linalg.inv(Prec)
     mu = Cov @ ((X_t.T @ y_t) / sigma2)
+
     L = np.linalg.cholesky(Cov)
     return mu + L @ rng.normal(size=2)
 
-def loglik(w, beta, sigma2):
-    X = np.column_stack([np.sin(w*x), np.cos(w*x)])
-    resid = y - X @ beta
-    return -0.5*np.sum(np.log(2*np.pi*sigma2*(s_obs**2)) + (resid**2)/(sigma2*(s_obs**2)))
-
 def sample_sigma2(w, beta):
-    X = np.column_stack([np.sin(w*x), np.cos(w*x)])
+    X = r(w)
     resid = y - X @ beta
     S = float(np.sum((resid / s_obs) ** 2))
     a = a0 + n/2
@@ -68,10 +55,6 @@ def sample_sigma2(w, beta):
     return 1.0 / rng.gamma(shape=a, scale=1.0/b)
 
 # страрт цепи
-w = w0
-logw = np.log(w)
-beta = sample_beta(w, 1.0)
-sigma2 = sample_sigma2(w, beta)
 
 n_iter = 30000
 burn = 6000
@@ -85,22 +68,29 @@ for t in range(n_iter):
     beta = sample_beta(w, sigma2)
     sigma2 = sample_sigma2(w, beta)
 
-    logw_prop = logw + rng.normal(scale=prop_sd)
-    w_prop = np.exp(logw_prop)
+    w_prop = w + rng.normal(scale=prop_sd)
 
-    lp_cur = loglik(w, beta, sigma2) + logprior_logw(logw)
-    lp_prop = loglik(w_prop, beta, sigma2) + logprior_logw(logw_prop)
+    if w_min <= w_prop <= w_max:
+        lp_cur = loglik(w, beta, sigma2)
+        lp_prop = loglik(w_prop, beta, sigma2)
 
-    if np.log(rng.uniform()) < (lp_prop - lp_cur):
-        logw = logw_prop
-        w = w_prop
-        acc += 1
+        if np.log(rng.uniform()) < (lp_prop - lp_cur):
+            w = w_prop
+            acc += 1
 
     if t >= burn and ((t - burn) % thin == 0):
         samples.append([beta[0], beta[1], w, np.sqrt(sigma2)])
 
+samples1 = []
+for i in samples:
+    spisok = []
+    for j in range(3):
+        spisok.append(i[j])
+    samples1.append(spisok)
+
+
+samples1 = np.array(samples1)
 samples = np.array(samples)
-acc_rate = acc / n_iter
 
 # Интервалы
 df = pd.DataFrame(samples)
@@ -113,3 +103,12 @@ print('из задания 2:')
 print('A = -0.35 +- 0.06')
 print('B = -0.99 +- 0.05')
 print('w = 0.703 +- 0.006')
+
+fig = corner.corner(
+    samples1,
+    labels=["A", "B", "w"],
+    quantiles=[0.025, 0.5, 0.975],
+    show_titles=True,
+    title_fmt=".4f",
+)
+plt.show()
